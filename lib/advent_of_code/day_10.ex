74d2f2d933acd72621c099e3775f3d13285725aa
dompdv <dompdv@yahoo.fr>
2023-11-17 12:40:34 +0100

    reverse sublist

--- lib/advent_of_code/day_10.ex
+++ lib/advent_of_code/day_10.ex
@@ -2,15 +2,32 @@ defmodule AdventOfCode.Day10 do
   def parse(args),
     do: args |> String.trim() |> String.split(",", trim: true) |> Enum.map(&String.to_integer/1)
 
-  def process(l, position, length, skip, list_length) do
-    for i <- 0..(length - 1), into: %{} do
-      {rem(position + i, list_length), rem(position + length - i, list_length)}
+  def reverse_sublist(l, position, length, skip, list_length) do
+    last_pos = position + length - 1
+    last_index = rem(last_pos, list_length)
+    overflow = last_pos >= list_length
+
+    for i <- 0..(list_length - 1), into: %{} do
+      cond do
+        overflow and (i <= last_index or i >= position) ->
+          {i, l[rem(last_pos - (i - position), list_length)]}
+
+        not overflow and (i >= position and i <= last_index) ->
+          {i, l[rem(last_pos - (i - position), list_length)]}
+
+        true ->
+          {i, l[i]}
+      end
     end
+  end
+
+  def process(l, position, length, skip, list_length) do
+    reverse_sublist(l, position, length, skip, list_length)
   end
 
   def part1(args) do
     list_length = 5
-    initial_list = for i <- 0..(list_length - 1), do: i
+    initial_list = for i <- 0..(list_length - 1), into: %{}, do: {i, i}
     args = "3,4,1,5\n"
 
     process(initial_list, 0, 3, 0, list_length)
