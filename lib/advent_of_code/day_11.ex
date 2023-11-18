d8d5e2ed4b743073d614203556d2201ecf5af539
dompdv <dompdv@yahoo.fr>
2023-11-18 11:12:09 +0100

    D12 P1

--- lib/advent_of_code/day_11.ex
+++ lib/advent_of_code/day_11.ex
@@ -1,4 +1,5 @@
 defmodule AdventOfCode.Day11 do
+  # Utilise les coordonnées cubiques (je dois avouer que j'ai trouvé un site très bien fait qui explique tout ça)
   def move("nw", {q, r, s}), do: {q, r - 1, s + 1}
   def move("n", {q, r, s}), do: {q + 1, r - 1, s}
   def move("ne", {q, r, s}), do: {q + 1, r, s - 1}
@@ -10,22 +11,26 @@ defmodule AdventOfCode.Day11 do
 
   def part1(args) do
     args
+    # On split la chaîne en une liste de directions
     |> String.trim()
     |> String.split(",", trim: true)
+    # On applique la fonction move à chaque direction
     |> Enum.reduce({0, 0, 0}, &move/2)
+    # On calcule la distance à l'arrivée
     |> dist()
   end
 
   def part2(args) do
     args
+    # On split la chaîne en une liste de directions
     |> String.trim()
     |> String.split(",", trim: true)
+    # On applique la fonction move à chaque direction, en gardant le max atteint jusqu'à présent
     |> Enum.reduce(
       {0, {0, 0, 0}},
       fn direction, {max_so_far, position} ->
         new_position = move(direction, position)
-        new_max = max(max_so_far, dist(new_position))
-        {new_max, new_position}
+        {max(max_so_far, dist(new_position)), new_position}
       end
     )
     |> elem(0)
--- lib/advent_of_code/day_12.ex
+++ lib/advent_of_code/day_12.ex
@@ -1,6 +1,34 @@
 defmodule AdventOfCode.Day12 do
-  def part1(_args) do
-    :ok
+  def parse(args) do
+    args
+    |> String.trim()
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      [id, neighbours] = String.split(line, " <-> ", trim: true)
+
+      {String.to_integer(id),
+       String.split(neighbours, ", ", trim: true) |> Enum.map(&String.to_integer/1)}
+    end)
+    |> Map.new()
+  end
+
+  def find_connex(_, [], visited), do: visited
+
+  # Maintien une liste de noeuds à visiter et une liste de noeuds visités
+  def find_connex(graph, [node | to_visit], visited) do
+    # Si déjà visité, on passe au suivant
+    if MapSet.member?(visited, node) do
+      find_connex(graph, to_visit, visited)
+    else
+      # Sinon, on ajoute les voisins à visiter et on ajoute le noeud aux visités
+      find_connex(graph, to_visit ++ graph[node], MapSet.put(visited, node))
+    end
+  end
+
+  def find_group(graph, node), do: find_connex(graph, [node], MapSet.new())
+
+  def part1(args) do
+    args |> parse() |> find_group(0) |> Enum.count()
   end
 
   def part2(_args) do
