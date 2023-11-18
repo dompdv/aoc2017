d27b52b71d47fbae96b4479ba9f36202d7c05ebf
dompdv <dompdv@yahoo.fr>
2023-11-18 11:23:04 +0100

    D12 P2

--- lib/advent_of_code/day_12.ex
+++ lib/advent_of_code/day_12.ex
@@ -17,12 +17,10 @@ defmodule AdventOfCode.Day12 do
   # Maintien une liste de noeuds à visiter et une liste de noeuds visités
   def find_connex(graph, [node | to_visit], visited) do
     # Si déjà visité, on passe au suivant
-    if MapSet.member?(visited, node) do
-      find_connex(graph, to_visit, visited)
-    else
+    if MapSet.member?(visited, node),
+      do: find_connex(graph, to_visit, visited),
       # Sinon, on ajoute les voisins à visiter et on ajoute le noeud aux visités
-      find_connex(graph, to_visit ++ graph[node], MapSet.put(visited, node))
-    end
+      else: find_connex(graph, to_visit ++ graph[node], MapSet.put(visited, node))
   end
 
   def find_group(graph, node), do: find_connex(graph, [node], MapSet.new())
@@ -31,7 +29,15 @@ defmodule AdventOfCode.Day12 do
     args |> parse() |> find_group(0) |> Enum.count()
   end
 
-  def part2(_args) do
-    :ok
+  def part2(args) do
+    graph = args |> parse()
+    n_nodes = map_size(graph)
+
+    Enum.reduce(0..(n_nodes - 1), {0, MapSet.new()}, fn node, {groups, visited} ->
+      if MapSet.member?(visited, node),
+        do: {groups, visited},
+        else: {groups + 1, MapSet.union(visited, MapSet.new(find_group(graph, node)))}
+    end)
+    |> elem(0)
   end
 end
