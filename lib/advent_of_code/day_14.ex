3bc62cb8c9e7a0997d593de0e3ce5e0db16bfbd1
dompdv <dompdv@yahoo.fr>
2023-11-19 15:25:58 +0100

    D14 P1

--- lib/advent_of_code/day_14.ex
+++ lib/advent_of_code/day_14.ex
@@ -1,6 +1,15 @@
 defmodule AdventOfCode.Day14 do
   def part1(_args) do
-    :ok
+    args = "xlqgujun"
+
+    for i <- 0..127 do
+      AdventOfCode.Day10.part2("#{args}-#{i}")
+      |> String.to_integer(16)
+      |> Integer.to_string(2)
+      |> String.to_charlist()
+      |> Enum.count(fn c -> c == ?1 end)
+    end
+    |> Enum.sum()
   end
 
   def part2(_args) do
