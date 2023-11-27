e3b3d30c0b3b7f611a4df4f69516e75385a6fcc1
dompdv <dompdv@yahoo.fr>
2023-11-27 16:49:44 +0100

    passage au PPCM

--- lib/advent_of_code/day_16.ex
+++ lib/advent_of_code/day_16.ex
@@ -21,47 +21,33 @@ defmodule AdventOfCode.Day16 do
     end)
   end
 
-  def print_s({n, low, high, mapping}) do
+  def print_s(mapping) do
     reverse = for {i, j} <- mapping, into: %{}, do: {j, i}
-    maps = for(i <- low..high, do: reverse[i] + ?a) |> to_string()
-    IO.puts("low: #{low}, high: #{high} mapping: #{maps}")
-    {n, low, high, mapping}
+    maps = for(i <- 0..(map_size(mapping) - 1), do: reverse[i] + ?a) |> to_string()
+    IO.puts("Mapping: #{maps}")
+    mapping
   end
 
-  def inital_state(n) do
-    # Size, low index, high index, map
-    {n, 0, n - 1, for(i <- 0..(n - 1), into: %{}, do: {i, i})}
+  def inital_state(n), do: for(i <- 0..(n - 1), into: %{}, do: {i, i})
+
+  def next_state({:spin, x}, mapping) do
+    n = map_size(mapping)
+    for {i, j} <- mapping, into: %{}, do: if(j < n - x, do: {i, j + x}, else: {i, j - n + x})
   end
 
-  def next_state({:spin, x}, {n, low, high, mapping}) do
-    new_mapping =
-      for {i, j} <- mapping do
-        if j <= high - x, do: {i, j}, else: {i, j - n}
+  def next_state({:exchange, a, b}, mapping) do
+    for {i, j} <- mapping do
+      cond do
+        j == a -> {i, b}
+        j == b -> {i, a}
+        true -> {i, j}
       end
-      |> Map.new()
-
-    {n, low - x, high - x, new_mapping}
+    end
+    |> Map.new()
   end
 
-  def next_state({:exchange, a, b}, {n, low, high, mapping}) do
-    new_mapping =
-      for {i, j} <- mapping do
-        cond do
-          j == a + low -> {i, b + low}
-          j == b + low -> {i, a + low}
-          true -> {i, j}
-        end
-      end
-      |> Map.new()
-
-    {n, low, high, new_mapping}
-  end
-
-  def next_state({:partner, a, b}, {n, low, high, mapping}) do
-    new_mapping =
-      mapping |> Map.put(a, mapping[b]) |> Map.put(b, mapping[a])
-
-    {n, low, high, new_mapping}
+  def next_state({:partner, a, b}, mapping) do
+    mapping |> Map.put(a, mapping[b]) |> Map.put(b, mapping[a])
   end
 
   def one_dance(steps, starting_point) do
@@ -80,57 +66,69 @@ defmodule AdventOfCode.Day16 do
     :ok
   end
 
-  def renormalize({_n, low, _high, mapping}) do
-    for {i, j} <- mapping, into: %{}, do: {i, j - low}
-  end
-
   def apply_permutation(mapping, permutation) do
     for {i, j} <- mapping, into: %{}, do: {i, permutation[j]}
   end
 
-  def print_p(mapping) do
-    reverse = for {i, j} <- mapping, into: %{}, do: {j, i}
-    maps = for(i <- 0..(map_size(reverse) - 1), do: reverse[i] + ?a) |> to_string()
-    IO.puts("mapping: #{maps}")
-    mapping
-  end
+  # find smallest common multiple of a list of integers
+  def gcd(a, 0), do: abs(a)
+  def gcd(a, b), do: gcd(b, rem(a, b))
+
+  def lcm(a, b), do: div(abs(a * b), gcd(a, b))
 
   def part2(args) do
     #    args = "s1,x3/4,pe/b"
-    args = List.duplicate("s1", 5) |> Enum.join(",")
-    n_dancers = 5
+    #    args = List.duplicate("s1", 4) |> Enum.join(",")
+    n_dancers = 16
 
     permutation =
       args
       |> parse_input()
       |> one_dance(inital_state(n_dancers))
-      |> renormalize()
-      |> print_p()
+      |> print_s()
       |> IO.inspect(label: "permutation")
 
-    initial_order = for(i <- 0..(n_dancers - 1), into: %{}, do: {i, i})
+    decomp_perm(permutation)
+    |> IO.inspect(label: "cycles")
+    |> Enum.map(&Enum.count/1)
+    |> IO.inspect(label: "cycle sizes")
+
+    initial_order = inital_state(n_dancers)
 
     loop_size =
       Enum.reduce_while(
         1..1_000_000_000,
-        initial_order |> print_p(),
+        initial_order |> print_s(),
         fn i, state ->
-          new_state = state |> apply_permutation(permutation) |> print_p()
+          new_state = state |> apply_permutation(permutation) |> print_s()
           if new_state == initial_order, do: {:halt, i}, else: {:cont, new_state}
         end
       )
       |> IO.inspect(label: "loop")
 
     Enum.reduce(
-      1..rem(1_000_000_000, loop_size - 1),
-      initial_order,
+      1..rem(1_000_000_000, loop_size),
+      initial_order |> print_s(),
       fn i, state ->
         IO.inspect(i)
-        state |> print_p() |> apply_permutation(permutation)
+        apply_permutation(state, permutation) |> print_s()
       end
     )
-    |> print_p()
 
     :ok
   end
+
+  def find_cycle(permutation, s, cycle) do
+    ps = permutation[s]
+    if ps in cycle, do: cycle, else: find_cycle(permutation, ps, [ps | cycle])
+  end
+
+  def decomp_perm(_permutation, [], cycles), do: cycles
+
+  def decomp_perm(permutation, [s | to_visit], cycles) do
+    cycle = find_cycle(permutation, s, [s]) |> IO.inspect(label: "cycle")
+    decomp_perm(permutation, to_visit -- cycle, [cycle | cycles])
+  end
+
+  def decomp_perm(permutation), do: decomp_perm(permutation, Map.values(permutation), [])
 end
